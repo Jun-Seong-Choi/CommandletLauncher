5646bf8e7a78f2dbc253cea4d89e4393aa0d3adb
Jun-Seong-Choi <155498957+Jun-Seong-Choi@users.noreply.github.com>
2024-02-08 21:28:32 +0900

    Create CommandletLauncherEditor.h

---
+++ Source/Public/CommandletLauncherEditor.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Framework/Docking/TabManager.h"
+
+static const FName CommandletLauncherEditorTabName("CommandletLauncherEditor");
+
+class FCommandletLauncherEditor : public FAssetEditorToolkit
+{
+public:
+
+	FCommandletLauncherEditor();
+	~FCommandletLauncherEditor();
+
+	/** IToolkit interface */
+	virtual FName GetToolkitFName() const override;
+	virtual FText GetBaseToolkitName() const override;
+	virtual FString GetWorldCentricTabPrefix() const override;
+	virtual FLinearColor GetWorldCentricTabColorScale() const override;
+	/** IToolkit interface */
+
+	virtual void RegisterTabSpawners(const TSharedRef<class FTabManager>& InTabManager) override;
+	virtual void UnregisterTabSpawners(const TSharedRef<class FTabManager>& InTabManager) override;
+
+	void InitEditor(const EToolkitMode::Type Mode, const TSharedPtr<class IToolkitHost>& InitToolkitHost);
+
+public:
+
+	static const FName TabID;
+
+private:
+
+	TSharedRef<SDockTab> SpawnTab(const FSpawnTabArgs& Args);
+
+private:
+
+	TArray<TSharedPtr<FName>> CommandletNames;
+};
