fad95460858ee1cb0b971acac299bac818c41d29
Jun-Seong-Choi <155498957+Jun-Seong-Choi@users.noreply.github.com>
2024-02-15 15:28:37 +0900

    Update CommandletLauncherEditor.cpp

--- Source/Private/CommandletLauncherEditor.cpp
+++ Source/Private/CommandletLauncherEditor.cpp
@@ -15,76 +15,89 @@ FCommandletLauncherEditor::~FCommandletLauncherEditor()
 
 FName FCommandletLauncherEditor::GetToolkitFName() const
 {
-    return FName(TEXT("CommandletLauncherEditor"));
+	return FName(TEXT("CommandletLauncherEditor"));
 }
 
 FText FCommandletLauncherEditor::GetBaseToolkitName() const
 {
-    return FText::FromString(FString(TEXT("CommandletLauncherEditor")));
+	return FText::FromString(FString(TEXT("CommandletLauncherEditor")));
 }
 
 FString FCommandletLauncherEditor::GetWorldCentricTabPrefix() const
 {
-    return FString(TEXT("CommandletLauncherEditor"));
+	return FString(TEXT("CommandletLauncherEditor"));
 }
 
 FLinearColor FCommandletLauncherEditor::GetWorldCentricTabColorScale() const
 {
-    return FLinearColor::White;
+	return FLinearColor::White;
 }
 
 void FCommandletLauncherEditor::RegisterTabSpawners(const TSharedRef<class FTabManager>& InTabManager)
 {
-    WorkspaceMenuCategory = InTabManager->AddLocalWorkspaceMenuCategory(LOCTEXT("WorkspaceMenu_CommandletLauncherEditor", "Commandlet Launcher Editor"));
-    auto WorkspaceMenuCategoryRef = WorkspaceMenuCategory.ToSharedRef();
+	WorkspaceMenuCategory = InTabManager->AddLocalWorkspaceMenuCategory(LOCTEXT("WorkspaceMenu_CommandletLauncherEditor", "Commandlet Launcher Editor"));
+	auto WorkspaceMenuCategoryRef = WorkspaceMenuCategory.ToSharedRef();
 
-    FAssetEditorToolkit::RegisterTabSpawners(InTabManager);
+	FAssetEditorToolkit::RegisterTabSpawners(InTabManager);
 
-    InTabManager->RegisterTabSpawner(TabID, FOnSpawnTab::CreateSP(this, &FCommandletLauncherEditor::SpawnTab))
-        .SetDisplayName(LOCTEXT("CommandletLauncherTab", "CommandletLauncher"))
-        .SetGroup(WorkspaceMenuCategoryRef)
-        .SetIcon(FSlateIcon(FAppStyle::GetAppStyleSetName(), "Icons.Info"));
+	InTabManager->RegisterTabSpawner(TabID, FOnSpawnTab::CreateSP(this, &FCommandletLauncherEditor::SpawnTab))
+		.SetDisplayName(LOCTEXT("CommandletLauncherTab", "CommandletLauncher"))
+		.SetGroup(WorkspaceMenuCategoryRef)
+		.SetIcon(FSlateIcon(FAppStyle::GetAppStyleSetName(), "Icons.Info"));
 }
 
 void FCommandletLauncherEditor::UnregisterTabSpawners(const TSharedRef<class FTabManager>& InTabManager)
 {
-    FAssetEditorToolkit::UnregisterTabSpawners(InTabManager);
+	FAssetEditorToolkit::UnregisterTabSpawners(InTabManager);
 
-    InTabManager->UnregisterTabSpawner(TabID);
+	InTabManager->UnregisterTabSpawner(TabID);
 }
 
 void FCommandletLauncherEditor::InitEditor(const EToolkitMode::Type Mode, const TSharedPtr<class IToolkitHost>& InitToolkitHost)
 {
-    const TSharedRef<FTabManager::FLayout> StandaloneDefaultLayout = FTabManager::NewLayout("Standalone_CommandletLauncher_Layout_v1")
-        ->AddArea
-        (
-            FTabManager::NewPrimaryArea()->SetOrientation(Orient_Horizontal)
-            ->Split
-            (
-                FTabManager::NewStack()
-                ->AddTab(TabID, ETabState::OpenedTab)
-            )
-        );
+	const TSharedRef<FTabManager::FLayout> StandaloneDefaultLayout = FTabManager::NewLayout("Standalone_CommandletLauncher_Layout_v1")
+		->AddArea
+		(
+			FTabManager::NewPrimaryArea()->SetOrientation(Orient_Horizontal)
+			->Split
+			(
+				FTabManager::NewStack()
+				->AddTab(TabID, ETabState::OpenedTab)
+			)
+		);
 
-    CreateInternalWidgets();
-    
-    const bool bCreateDefaultStandaloneMenu = true;
-    const bool bCreateDefaultToolbar = true;
-    FAssetEditorToolkit::InitAssetEditor(Mode, InitToolkitHost, CommandletLauncherEditorTabName, StandaloneDefaultLayout, bCreateDefaultStandaloneMenu, bCreateDefaultToolbar, nullptr);
+	CreateInternalWidgets();
+
+	USkeletalMeshComponent* Object = NewObject<USkeletalMeshComponent>();
+
+	const bool bCreateDefaultStandaloneMenu = true;
+	const bool bCreateDefaultToolbar = true;
+	FAssetEditorToolkit::InitAssetEditor(Mode, InitToolkitHost, CommandletLauncherEditorTabName, StandaloneDefaultLayout, bCreateDefaultStandaloneMenu, bCreateDefaultToolbar, Object);
 }
 
-TSharedRef<SDockTab> FCommandletLauncherEditor::SpawnTab(const FSpawnTabArgs& Args)
+void FCommandletLauncherEditor::ExecuteCommandlet(const FString& InCommandletName, const FString& InCommandletArgs)
 {
-    return SNew(SDockTab)
-        .Label(LOCTEXT("CommandletLauncherEditor", "CommandletLauncher"))
-        [
-            EditorWidget.ToSharedRef()            
-        ];
+	if (!CommandletLauncher.IsValid())
+	{
+		CommandletLauncher = MakeShared<FCommandletLauncher>();
+	}
+
+	CommandletLauncher->CreateNewCommandletProcess(InCommandletName, InCommandletArgs);
+}
+
+TSharedRef<SDockTab> FCommandletLauncherEditor::SpawnTab(const FSpawnTabArgs& InArgs)
+{
+	return SNew(SDockTab)
+		.Label(LOCTEXT("CommandletLauncherEditor", "CommandletLauncher"))
+		[
+			EditorWidget.ToSharedRef()
+		];
 }
 
 void FCommandletLauncherEditor::CreateInternalWidgets()
 {
-    EditorWidget = SNew(SCommandletLauncherEditorWidget)
-        .Editor(SharedThis(this));
+	EditorWidget = SNew(SCommandletLauncherEditorWidget)
+		.Editor(SharedThis(this));
 }
+
 #undef LOCTEXT_NAMESPACE
